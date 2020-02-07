7da924c463b4dd2df0a4a57ea6a9d72be217f3a2
banko2 <60343173+banko2@users.noreply.github.com>
2020-02-07 11:03:02 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+            <h1 style="background-color: red ";>Woman Gallery </h1>
+<img src="Wgalery.jpg">
+<h2> Choose Women </h2>
+
+      </header> 
+
+<div class="gallery">
+  <a class="button"  href="ital3.mp4">PUSH!</a>
+    <img src="ital2.jpg">
+  <div class="desc">Woman</div>
+</div>
+
+<div class="gallery">
+  <a class="button" href="image1.mp4">ПИПНИ!</a>
+    <img src="ital1.jpg">
+  <div class="desc">Woman1</div>
+</div>
+
+<div class="gallery">
+  <a class="button" href="bulg.mp4">Take me!</a>
+    <img src="bulg.jpg">  
+  <div class="desc">Woman2</div>
+</div>
+
+<div class="gallery">
+  <a class="button" href="image3.mp4">PULL</a>
+    <img src="usa.jpg">  
+  <div class="desc">Woman3</div>
+  </div>
+
+  <div class="gallery">
+  <a class="button" href="image12.mp4">Take off</a>
+    <img src="image12.jpg" >  
+  <div class="desc">Woman4</div>
+  </div>
+ 
+</body>
+</html>
